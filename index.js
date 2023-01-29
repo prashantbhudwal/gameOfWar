0683226a5d4365b5b5db449c8f2ddc9aabcb51e3
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-01-29 11:44:46 +0530

    Draw 'n' cards from the same deck that's fetched.

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-import { getCards, getNewDeck, getDeckId } from "./deckLogic.js";
+import { getCards, getNewDeck } from "./deckLogic.js";
 import { logIt, getJson } from "./utils.js";
 
 const elements = {
   getDeckBtn: document.getElementById("get-new-deck-btn"),
   drawCardsBtn: document.getElementById("draw-cards-btn"),
 };
-
+elements.drawCardsBtn.disabled = true;
+const enableButton = function () {
+  return (elements.drawCardsBtn.disabled = false);
+};
 const numberOfCards = 2;
 
-const drawCardsHandler = () =>
+let newDeck;
+
+const newDeckHandler = () =>
   getNewDeck()
     .then(getJson)
-    .then((deck) => getCards(deck, numberOfCards))
-    .then(getJson)
-    .then(logIt);
+    .then((json) => (newDeck = json))
+    .then(logIt)
+    .then(enableButton());
 
-const newDeckHandler = () => getNewDeck().then(getJson).then(logIt);
+const drawCardsHandler = () =>
+  getCards(newDeck, numberOfCards).then(getJson).then(logIt);
 
 elements.getDeckBtn.addEventListener("click", newDeckHandler);
 
