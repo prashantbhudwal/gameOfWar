62cd7977283e0c62fbf8309fb565fa17c14866cd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-01-29 08:52:03 +0530

    Fetch a deck & log its Id

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const fetchCards = function fetchADeckOfCardsFromAPI() {
+const fetchDeck = function fetchADeckOfCardsFromAPI() {
   const apiUrl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/";
-  fetch(apiUrl)
-    .then((deck) => deck.json())
-    .then((deckJson) => console.log(deckJson));
+  return fetch(apiUrl).then((deck) => deck.json());
 };
 
-document
-  .getElementById("fetch-cards-btn")
-  .addEventListener("click", fetchCards);
+const getDeckId = function getTheIdOfTheDeck(json) {
+  return json.deck_id;
+};
+
+const logIt = function logAnything(prompt) {
+  console.log(prompt);
+};
+
+fetchDeck().then(getDeckId).then(logIt);
+
+document.getElementById("fetch-cards-btn").addEventListener("click", fetchDeck);
