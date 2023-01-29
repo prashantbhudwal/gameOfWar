3bc717fd573af2b682e1d6b70e0e13a97d858484
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-01-29 10:29:38 +0530

    Get n cards from the deck

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const fetchDeck = function fetchADeckOfCardsFromAPI() {
-  const apiUrl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/";
-  return fetch(apiUrl).then((deck) => deck.json());
+import DeckApi from "./DeckApi.js";
+
+const getJson = function (data) {
+  return data.json();
+};
+
+const getNewDeck = function fetchADeckOfCardsFromAPI() {
+  const url = DeckApi.getNewDeckEndpoint();
+  return fetch(url).then(getJson);
 };
 
 const getDeckId = function getTheIdOfTheDeck(json) {
@@ -11,6 +17,21 @@ const logIt = function logAnything(prompt) {
   console.log(prompt);
 };
 
-fetchDeck().then(getDeckId).then(logIt);
+const getCards = function getCardsFromTheDeck(deck, numberOfCards) {
+  const deckId = getDeckId(deck);
+  const cards = numberOfCards;
+  return fetch(DeckApi.getDrawCardsEndpoint(deckId, cards));
+};
+const numberOfCards = 2;
 
-document.getElementById("fetch-cards-btn").addEventListener("click", fetchDeck);
+getNewDeck()
+  .then((deck) => getCards(deck, numberOfCards))
+  .then(getJson)
+  .then(logIt);
+
+const clickHandler = () =>
+  getNewDeck().then(getJson).then(getDeckId).then(logIt);
+
+document
+  .getElementById("fetch-cards-btn")
+  .addEventListener("click", clickHandler);
